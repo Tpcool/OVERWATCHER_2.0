d354ed130d38617b1dfd3c4f9a906861d663d648
Petr Sedláček <petr.spelos@gmail.com>
2018-09-14 17:03:32 +0200

    Add InversionOfControl

---
+++ src/DiscordBot/InversionOfControl.cs
@@ -0,0 +1,37 @@
+using Lamar;
+
+namespace DiscordBot
+{
+    public static class InversionOfControl
+    {
+        private static Container container;
+
+        public static Container Container
+        {
+            get
+            {
+                return GetOrInitContainer();
+            }
+        }
+
+        private static Container GetOrInitContainer()
+        {
+            if(container is null)
+            {
+                InitializeContainer();
+            }
+
+            return container;
+        }
+
+        public static void InitializeContainer()
+        {
+            container = new Container(c =>
+            {
+                // Add the types you need
+                // c.For<YourInterface>().Use<YourConcretion>();
+                // c.ForSingletonOf<YourSingletonType>().UseIfNone<YourSingletonType>();
+            });
+        }
+    }
+}
