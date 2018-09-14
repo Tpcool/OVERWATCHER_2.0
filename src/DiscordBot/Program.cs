aa8492a0de89dad863338ee75f1df49d5038337e
Petr Sedláček <petr.spelos@gmail.com>
2018-09-14 16:49:51 +0200

    Implement C# latest minor

--- src/DiscordBot/Program.cs
+++ src/DiscordBot/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace DiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Task.Delay(-1);
         }
     }
 }
